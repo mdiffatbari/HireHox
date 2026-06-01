90725327d5c3a9dc43eabf1afeeb655eaf3aa428
mdiffatbari <iffatbaribappy1999@gmail.com>
2026-06-01 13:55:06 +0600

    pages added on navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,7 @@
+import Blogs from '@/pages/Blogs/Blogs';
+import Contact from '@/pages/Contact/Contact';
 import Errorpage from '@/pages/Errorpage/Errorpage';
+import FindJobs from '@/pages/FindJobs/FindJobs';
 import Home from '@/pages/Home/Home';
 import Root from '@/pages/Root/Root';
 import React from 'react';
@@ -9,12 +12,24 @@ export const router = createBrowserRouter([
     path: "/",
     errorElement: <Errorpage></Errorpage>,
     Component: Root,
-    children:[
-        {
-            index: true,
-            path: "/",
-            Component: Home
-        }
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home
+      },
+      {
+        path: "/findJobs",
+        Component: FindJobs
+      },
+      {
+        path: "/blogs",
+        Component: Blogs
+      },
+      {
+        path: "/contact",
+        Component: Contact
+      }
     ]
   },
 ]);
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '../ui/button';
-import { Link } from 'react-router';
+import { NavLink } from 'react-router';
+import "./navbar.css"
 
 
 const Navbar = () => {
@@ -8,25 +9,30 @@ const Navbar = () => {
 
 return (
   <nav>
-    <div className='py-3'>
+    <div className='py-5'>
       <div className='flex w-11/12 mx-auto items-center justify-between'>
         <div className="navbar-first">
           <h1 className='text-2xl font-bold'>Hirehox</h1>
         </div>
         <div className="navbar-middle">
-          <ul className='flex gap-8'>
-            <li>
-              <Link to="/">Home</Link>
+          <ul className='flex gap-8 font-semibold'>
+
+            <li className='hover:text-blue-600'>
+              <NavLink className="menu-item" to="/">Home</NavLink>
             </li>
-            <li>
-              <Link to="/">Find Jobs</Link>
+            
+            <li className='hover:text-blue-600'>
+              <NavLink className="menu-item" to="/findJobs">Find Jobs</NavLink>
             </li>
-            <li>
-              <Link to="/">Blogs</Link>
+
+            <li className='hover:text-blue-600'>
+              <NavLink className="menu-item" to="/blogs">Blog</NavLink>
             </li>
-            <li>
-              <Link to="/">Contact us</Link>
+
+            <li className='hover:text-blue-600'>
+              <NavLink className="menu-item" to="/contact">Contact Us</NavLink>
             </li>
+
           </ul>
         </div>
         <div className="navbar-lst">
