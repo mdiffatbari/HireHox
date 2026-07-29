46a1c6907db9f02f875759167d93cab65958fa19
mdiffatbari <iffatbaribappy1999@gmail.com>
2026-07-29 07:02:26 +0600

    hero section font change

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
                                 AI-Powered Job Platform
                             </span>
 
-                            <h1 className="mt-6 text-5xl lg:text-8xl font-extrabold leading-tight text-slate-900">
+                            <h1 className="mt-6 text-5xl lg:text-8xl font-bold leading-tight text-slate-900 g-font">
                                 Find Your Next
                                 
                                 <span className="text-blue-600"> Career Move </span>
